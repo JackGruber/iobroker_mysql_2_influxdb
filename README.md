5a44359eddc693fd8b6a6f38e8ec9df51717bcfd
JackGruber <24863925+JackGruber@users.noreply.github.com>
2020-08-11 20:34:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# migrate_iobroker_mysql_to_influxdb
+Python script to migrate ioBroker MySQL (SQL History Adapter) data to influxDB (Logging data with influxDB)
+
+Based on a script from UlliJ from the [ioBroker Forum](https://forum.iobroker.net/topic/12482/frage-migrate-mysql-nach-influxdb/26)
+
+I have used the script to migrate 22 million records from a MySQL DB running on a Rasperry Pi to an influxDB on a Synology. 
---
+++ migrate.py
@@ -0,0 +1,116 @@
+### MySQL DB info ###
+import MySQLdb
+MYSQL_CONNECTION = MySQLdb.connect(host="xxxxxxxxxxxxxxxxxxxxxx",  # your host
+                                   port = 3306,                    # your DB port
+                                   user="xxxx",                    # your username
+                                   passwd="xxxxxxxxxxxxxxxxxxxx",  # your password
+                                   db="xxxxxxxx")                  # name of the database
+ 
+
+# InfluxDB info #
+from influxdb import InfluxDBClient
+INFLUXDB_CONNECTION = InfluxDBClient(host='xxxxxxxxxxxxxxxxxxxxxx', # your host
+                                     port=8086,                     # your DB port
+                                     username='xxxxxxxx',           # your username
+                                     password='xxxxxxxxxxxxxxxx',   # your password
+                                     database='xxxxxxxx')           # name of the database
+#####
+
+
+
+import time
+
+# dictates how columns will be mapped to key/fields in InfluxDB
+SCHEMA = {
+    "time_column": "time", # the column that will be used as the time stamp in influx
+    "columns_to_fields" : ["ack","q", "from","value"], # columns that will map to fields 
+    # "columns_to_tags" : ["",...], # columns that will map to tags
+    "table_name_to_measurement" : "name", # table name that will be mapped to measurement
+    }
+
+ 
+
+#####
+#Generates an collection of influxdb points from the given SQL records
+#####
+def generate_influx_points(records):
+    influx_points = []
+    for record in records:
+        #tags = {}, 
+        fields = {}
+        #for tag_label in SCHEMA['columns_to_tags']:
+        #   tags[tag_label] = record[tag_label]
+        for field_label in SCHEMA['columns_to_fields']:
+            fields[field_label] = record[field_label]
+        influx_points.append({
+            "measurement": record[SCHEMA['table_name_to_measurement']],
+            #"tags": tags,
+            "time": record[SCHEMA['time_column']],
+            "fields": fields
+        })
+
+    return influx_points
+
+def query_metrics(table):
+    MYSQL_CURSOR.execute("SELECT name, id FROM datapoints WHERE id IN(SELECT DISTINCT id FROM " + table + ")")
+    rows = MYSQL_CURSOR.fetchall()
+    print('Total metrics in ' + table + ": " + str(MYSQL_CURSOR.rowcount))
+    return rows
+
+def migrate_datapoints(table):
+    query_max_rows = 100000 # prevent run out of mermory limit on SQL DB
+    process_max_rows = 1000     
+    
+    migrated_datapoints = 0    
+    metrics = query_metrics(table)
+    metric_nr = 0
+    metric_count = str(len(metrics))
+    processed_rows = 0
+    for metric in metrics:
+        metric_nr += 1
+        print(metric['name'] + " (" + str(metric_nr) + "/" + str(metric_count) + ")")
+        
+        start_row = 0
+        processed_rows = 0
+        while True:
+            query  = """SELECT d.name,
+                                m.ack AS 'ack',
+                                (m.q*1.0) AS 'q',
+                                s.name AS "from",
+                                (m.val*1.0) AS 'value',
+                                (m.ts*1000000) AS'time'
+                                FROM """ + table + """ AS m
+                                LEFT JOIN datapoints AS d ON m.id=d.id
+                                LEFT JOIN sources AS s ON m._from=s.id
+                                WHERE q=0 AND d.id = """ + str(metric['id']) + """
+                                ORDER BY m.ts desc
+                                LIMIT """ + str(start_row)+ """, """ + str(query_max_rows)
+            MYSQL_CURSOR.execute(query)
+            if MYSQL_CURSOR.rowcount == 0:
+                break
+            
+            # process x records at a time
+            while True:            
+                selected_rows = MYSQL_CURSOR.fetchmany(process_max_rows)
+                if len(selected_rows) == 0:
+                    break
+                
+                print(f"Processing row {processed_rows + 1:,} to {processed_rows + len(selected_rows):,} from LIMIT {start_row:,} / {start_row + query_max_rows:,} " + table + " - " + metric['name'] + " (" + str(metric_nr) + "/" + str(metric_count) + ")")
+                migrated_datapoints += len(selected_rows)
+                INFLUXDB_CONNECTION.write_points(generate_influx_points(selected_rows))
+                processed_rows += len(selected_rows)
+                
+            start_row += query_max_rows
+        print("")
+        
+    return migrated_datapoints
+
+MYSQL_CURSOR = MYSQL_CONNECTION.cursor(cursorclass=MySQLdb.cursors.DictCursor) 
+migrated = 0
+migrated += migrate_datapoints("ts_number")
+migrated += migrate_datapoints("ts_bool")
+migrated += migrate_datapoints("ts_string")
+print(f"Migrated: {migrated:,}")
+
+
+MYSQL_CONNECTION.close()
